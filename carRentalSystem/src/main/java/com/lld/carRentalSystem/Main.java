8726d39f93d36dee0e80b54972177410fc612889
codeX1616 <pratikjoshi1616@gmail.com>
2023-08-30 15:05:57 +0530

    Refactoring code in main

--- carRentalSystem/src/main/java/com/lld/carRentalSystem/Main.java
+++ carRentalSystem/src/main/java/com/lld/carRentalSystem/Main.java
@@ -16,40 +16,28 @@ import java.util.List;
 public class Main {
 
     public static void main(String args[]) {
-
         List<User> users = addUsers();
         List<Vehicle> vehicles = addVehicles();
         List<Store> stores = addStores(vehicles);
 
         VehicleRentalSystem rentalSystem = new VehicleRentalSystem(stores, users);
 
-        //0. User comes
         User user = users.get(0);
+        Location location = new Location(123, "city", "state", "country");
 
-        //1. user search store based on location
-        Location location = new Location(403012, "Bangalore", "Karnataka", "India");
         Store store = rentalSystem.getStore(location);
-
-        //2. get All vehicles you are interested in (based upon different filters)
         List<Vehicle> storeVehicles = store.getVehicles(VehicleType.CAR);
 
-        //3.reserving the particular vehicle
         Reservation reservation = store.createReservation(storeVehicles.get(0), users.get(0));
 
-        //4. generate the bill
         Bill bill = new Bill();
         bill.setReservation(reservation);
 
-        //5. make payment
         Payment payment = new Payment();
         payment.payBill(bill);
 
-        //6. trip completed, submit the vehicle and close the reservation
         store.completeReservation(reservation.getResId());
-
     }
-
-
 
     public static List<Vehicle> addVehicles(){
 
