0e6b08e563d3084d0cf3fa941b7458f09b6db647
codeX1616 <pratikjoshi1616@gmail.com>
2023-09-07 16:49:19 +0530

    codeX1616-Adding Java doc comments for bill payment

--- carRentalSystem/src/main/java/com/lld/carRentalSystem/entities/billingDetails/Payment.java
+++ carRentalSystem/src/main/java/com/lld/carRentalSystem/entities/billingDetails/Payment.java
@@ -2,6 +2,10 @@ package com.lld.carRentalSystem.entities.billingDetails;
 
 public class Payment {
 
+    /**
+     * Method to perform bill payment
+     * @param bill
+     */
     public void payBill(Bill bill) {
         // Perform payment process
     }
