a69ddaa2653a367abf83e619ee03512a913a8399
codeX1616 <pratikjoshi1616@gmail.com>
2023-09-07 16:52:59 +0530

    codeX1616 - Adding java docs for Store class

--- carRentalSystem/src/main/java/com/lld/carRentalSystem/entities/storeDetails/Store.java
+++ carRentalSystem/src/main/java/com/lld/carRentalSystem/entities/storeDetails/Store.java
@@ -21,10 +21,20 @@ public class Store {
     Location storeLocation;
     List<Reservation> reservations;
 
+    /**
+     * Method to set inventory management with vehicle list
+     * @param vehicleList
+     */
     public void setInventoryManagement(List<Vehicle> vehicleList) {
         this.inventoryManagement = new VehicleInventoryManagement(vehicleList);
     }
 
+    /**
+     * MEthod for creating reservation
+     * @param vehicle
+     * @param user
+     * @return {@link Reservation}
+     */
     public Reservation createReservation(Vehicle vehicle, User user) {
         Reservation reservation = new Reservation();
         int reservationId = reservation.createReserve(user, vehicle);
@@ -33,15 +43,29 @@ public class Store {
         return reservation;
     }
 
+    /**
+     * Method to complete reservation
+     * @param reservationId
+     * @return
+     */
     public boolean completeReservation(int reservationId) {
         // Logic for completing reservation
         return true;
     }
 
+    /**
+     * Get vehicles from inventory management
+     * @param type
+     * @return {@link List<Vehicle>}
+     */
     public List<Vehicle> getVehicles(VehicleType type) {
         return inventoryManagement.getVehicles();
     }
 
+    /**
+     * Method to set vehicle list to inventory management
+     * @param vehicles
+     */
     public void setVehicles(List<Vehicle> vehicles) {
         inventoryManagement = new VehicleInventoryManagement(vehicles);
     }
