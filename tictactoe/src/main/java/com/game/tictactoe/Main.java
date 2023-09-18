c6a6ab1232997e9c065172d4922a3d41745224e0
codeX1616 <pratikjoshi1616@gmail.com>
2023-09-18 21:33:03 +0530

    codeX1616-Adding java docs

--- tictactoe/src/main/java/com/game/tictactoe/Main.java
+++ tictactoe/src/main/java/com/game/tictactoe/Main.java
@@ -2,6 +2,10 @@ package com.game.tictactoe;
 
 public class Main {
 
+    /**
+     * Main method for tic-tac-toe game
+     * @param args
+     */
     public static void main(String args[]) {
         TicTacToeGame game = new TicTacToeGame();
         game.initializeGame();
