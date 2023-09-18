6be793cac23c03c2a6228f39713d4c85116e3671
codeX1616 <pratikjoshi1616@gmail.com>
2023-09-18 21:45:33 +0530

    codeX1616-Tic Tac Toe game initialization

--- tictactoe/src/main/java/com/game/tictactoe/TicTacToeGame.java
+++ tictactoe/src/main/java/com/game/tictactoe/TicTacToeGame.java
@@ -1,14 +1,38 @@
 package com.game.tictactoe;
 
+import com.game.tictactoe.entities.Board;
+import com.game.tictactoe.entities.Player;
+import com.game.tictactoe.entities.piece.PlayingPiece;
+import com.game.tictactoe.entities.piece.PlayingPieceO;
+import com.game.tictactoe.entities.piece.PlayingPieceX;
 import lombok.NoArgsConstructor;
+
+import java.util.Deque;
+import java.util.LinkedList;
 
 @NoArgsConstructor
 public class TicTacToeGame {
+
+    Deque<Player> players;
+    Board gameBoard;
 
     /**
      * Method to initialize tic-tac-toe game
      */
     public void initializeGame() {
+
+        players = new LinkedList<>();
+        PlayingPiece crossPiece = new PlayingPieceX();
+        Player player1 = new Player("Player1", crossPiece);
+
+        PlayingPiece noughtsPiece = new PlayingPieceO();
+        Player player2 = new Player("Player2", noughtsPiece);
+
+        players.add(player1);
+        players.add(player2);
+
+        //initializeBoard
+        gameBoard = new Board(3);
 
     }
 
