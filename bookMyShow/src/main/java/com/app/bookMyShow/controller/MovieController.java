6564e45ad164bb0c1b194fb1b49b8251459804e6
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-08 22:58:38 +0530

    codeX1616-Adding TODOs to methods

--- bookMyShow/src/main/java/com/app/bookMyShow/controller/MovieController.java
+++ bookMyShow/src/main/java/com/app/bookMyShow/controller/MovieController.java
@@ -3,13 +3,22 @@ package com.app.bookMyShow.controller;
 import com.app.bookMyShow.entity.Movie;
 import com.app.bookMyShow.entity.enums.City;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieController {
 
     public Movie getMovieByName(String movieName) {
+        // TODO - add logic to get movie by name
         return null;
     }
 
     public void addMovie(Movie movie, City city) {
+        // TODO - Add logic to add movie
+    }
 
+    public List<Movie> getMoviesByCity(City userCity) {
+        // TODO - Add logic to get movies by city
+        return new ArrayList<>();
     }
 }
--- bookMyShow/src/main/java/com/app/bookMyShow/controller/TheatreController.java
+++ bookMyShow/src/main/java/com/app/bookMyShow/controller/TheatreController.java
@@ -1,10 +1,20 @@
 package com.app.bookMyShow.controller;
 
+import com.app.bookMyShow.entity.Movie;
+import com.app.bookMyShow.entity.Show;
 import com.app.bookMyShow.entity.Theatre;
 import com.app.bookMyShow.entity.enums.City;
 
-public class TheatreController {
-    public void addTheatre(Theatre inoxTheatre, City bangalore) {
+import java.util.List;
+import java.util.Map;
 
+public class TheatreController {
+    public void addTheatre(Theatre theatre, City city) {
+        // TODO - Add logic to add theatre details
+    }
+
+    public Map<Theatre, List<Show>> getAllShow(Movie interestedMovie, City userCity) {
+        // TODO - Add logic to get all show details
+        return null;
     }
 }
