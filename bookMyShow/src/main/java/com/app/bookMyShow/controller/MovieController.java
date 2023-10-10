97d14fb4863e616ab5d2bd14b04dce83e0ceb8d5
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-10 23:54:23 +0530

    codeX1616-Added members and constructor

--- bookMyShow/src/main/java/com/app/bookMyShow/controller/MovieController.java
+++ bookMyShow/src/main/java/com/app/bookMyShow/controller/MovieController.java
@@ -4,9 +4,20 @@ import com.app.bookMyShow.entity.Movie;
 import com.app.bookMyShow.entity.enums.City;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MovieController {
+
+    Map<City, List<Movie>> cityVsMovies;
+    List<Movie> allMovies;
+
+    MovieController(){
+        cityVsMovies = new HashMap<>();
+        allMovies = new ArrayList<>();
+    }
+
 
     public Movie getMovieByName(String movieName) {
         // TODO - add logic to get movie by name
