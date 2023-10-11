b9eeb2a5b0fe19fffcde307891a46e66002d4d16
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-11 23:05:45 +0530

    Add theatre method

--- bookMyShow/src/main/java/com/app/bookMyShow/controller/TheatreController.java
+++ bookMyShow/src/main/java/com/app/bookMyShow/controller/TheatreController.java
@@ -5,12 +5,22 @@ import com.app.bookMyShow.entity.Show;
 import com.app.bookMyShow.entity.Theatre;
 import com.app.bookMyShow.entity.enums.City;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class TheatreController {
+
+    Map<City, List<Theatre>> cityVsTheatre;
+    List<Theatre> allTheatre;
+
     public void addTheatre(Theatre theatre, City city) {
-        // TODO - Add logic to add theatre details
+        allTheatre.add(theatre);
+
+        List<Theatre> theatres = cityVsTheatre.getOrDefault(city, new ArrayList<>());
+        theatres.add(theatre);
+        cityVsTheatre.put(city, theatres);
+
     }
 
     public Map<Theatre, List<Show>> getAllShow(Movie interestedMovie, City userCity) {
