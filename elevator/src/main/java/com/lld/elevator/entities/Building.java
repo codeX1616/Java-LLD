3b060045de20a08e0f59bd13729c231af815a396
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-18 22:04:28 +0530

    Adding methods to Building class

--- elevator/src/main/java/com/lld/elevator/entities/Building.java
+++ elevator/src/main/java/com/lld/elevator/entities/Building.java
@@ -1,10 +1,30 @@
 package com.lld.elevator.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class Building {
 
-    public Building(List<Floor> floorList) {
+    private List<Floor> floorList;
 
+    public Building(List<Floor> floorList) {
+        this.floorList = floorList;
     }
+
+    public void addFloors(Floor newFloor) {
+        floorList.add(newFloor);
+    }
+
+    public void removeFloors(Floor removeFloor) {
+        floorList.remove(removeFloor);
+    }
+
+    List<Floor> getAllFloorList() {
+        return floorList;
+    }
+
 }
