57e3b51bd4e47feb569ebcbb1a8bce2b08f8f675
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-19 22:59:40 +0530

    Adding methods to Floor class

--- elevator/src/main/java/com/lld/elevator/entities/Floor.java
+++ elevator/src/main/java/com/lld/elevator/entities/Floor.java
@@ -2,7 +2,16 @@ package com.lld.elevator.entities;
 
 public class Floor {
 
-    public Floor(Integer i) {
+    private Integer floorNumber;
+    private ExternalDispatcher externalDispatcher;
 
+    public Floor(Integer floorNumber) {
+        this.floorNumber = floorNumber;
+        this.externalDispatcher = new ExternalDispatcher();
     }
+
+    public void pressButton(Direction direction) {
+        externalDispatcher.submitExternalRequest(floorNumber, direction);
+    }
+
 }
