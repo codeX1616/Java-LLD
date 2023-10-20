d101e309069c6bca687de658573e98b5124d29a0
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-20 23:54:55 +0530

    adding submit method logic

--- elevator/src/main/java/com/lld/elevator/entities/ExternalDispatcher.java
+++ elevator/src/main/java/com/lld/elevator/entities/ExternalDispatcher.java
@@ -1,8 +1,24 @@
 package com.lld.elevator.entities;
 
+import com.lld.elevator.controller.ElevatorController;
+
+import java.util.List;
+
 public class ExternalDispatcher {
 
+    private List<ElevatorController> elevatorControllerList = ElevatorCreator.elevatorControllerList;
+
     public void submitExternalRequest(Integer floorNumber, Direction direction) {
+        for(ElevatorController elevatorController : elevatorControllerList) {
+
+            int elevatorID = elevatorController.elevatorCar.id;
+            if (elevatorID%2==1 && floor%2==1){
+                elevatorController.submitExternalRequest(floor,direction);
+            } else if(elevatorID%2==0 && floor%2==0){
+                elevatorController.submitExternalRequest(floor,direction);
+
+            }
+        }
 
     }
 
