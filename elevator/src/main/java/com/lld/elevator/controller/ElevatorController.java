dca977d477d0650defb5bd80efd3f331e6f0b68f
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-20 23:57:31 +0530

    Creating Elevator controller

---
+++ elevator/src/main/java/com/lld/elevator/controller/ElevatorController.java
@@ -0,0 +1,21 @@
+package com.lld.elevator.controller;
+
+import com.lld.elevator.entities.Direction;
+import com.lld.elevator.entities.ElevatorCar;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.PriorityQueue;
+
+@Getter
+@Setter
+public class ElevatorController {
+
+    private PriorityQueue<Integer> upMinPQ;
+    private PriorityQueue<Integer> downMaxPQ;
+    private ElevatorCar elevatorCar;
+
+    public void submitExternalRequest(Integer floor, Direction direction) {
+
+    }
+}
