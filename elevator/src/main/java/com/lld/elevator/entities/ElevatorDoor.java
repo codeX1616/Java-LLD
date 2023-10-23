c04e6dfe9f8a940b8b874c5510c80938e15d7ac2
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-23 23:51:10 +0530

    Adding open and close door functionalities

--- elevator/src/main/java/com/lld/elevator/entities/ElevatorDoor.java
+++ elevator/src/main/java/com/lld/elevator/entities/ElevatorDoor.java
@@ -1,4 +1,13 @@
 package com.lld.elevator.entities;
 
 public class ElevatorDoor {
+
+    public void openDoor(){
+        System.out.println("Opening the Elevator door ");
+    }
+
+    public void closeDoor(){
+        System.out.println("Closing the Elevator door");
+    }
+
 }
