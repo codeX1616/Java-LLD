8db728ce7e72bf904329eafacc4d387734dac659
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-23 23:43:02 +0530

    Adding static block functionality

--- elevator/src/main/java/com/lld/elevator/controller/ElevatorCreator.java
+++ elevator/src/main/java/com/lld/elevator/controller/ElevatorCreator.java
@@ -1,10 +1,27 @@
 package com.lld.elevator.controller;
 
+import com.lld.elevator.entities.ElevatorCar;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class ElevatorCreator {
+
     public static List<ElevatorController> elevatorControllerList;
+
+    static {
+
+        ElevatorCar elevatorCar1 = new ElevatorCar();
+        elevatorCar1.setId(1);
+        ElevatorController controller1 = new ElevatorController(elevatorCar1);
+
+        ElevatorCar elevatorCar2 = new ElevatorCar();
+        elevatorCar1.setId(2);
+        ElevatorController controller2 = new ElevatorController(elevatorCar2);
+
+        elevatorControllerList.add(controller1);
+        elevatorControllerList.add(controller2);
+    }
+
 }
