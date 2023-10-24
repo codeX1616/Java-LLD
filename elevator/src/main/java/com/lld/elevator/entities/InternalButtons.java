bb816daec0fe0e94449d2b1bc0cf012e35afab8b
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-24 23:45:39 +0530

    Refactoring Internal buttons

--- elevator/src/main/java/com/lld/elevator/entities/InternalButtons.java
+++ elevator/src/main/java/com/lld/elevator/entities/InternalButtons.java
@@ -1,4 +1,21 @@
 package com.lld.elevator.entities;
 
+import com.lld.elevator.dispatcher.InternalDispatcher;
+
 public class InternalButtons {
+
+    InternalDispatcher dispatcher = new InternalDispatcher();
+
+    Integer[] availableButtons = {1,2,3,4,5,6,7,8,9,10};
+    int buttonSelected;
+
+    public void pressButton(Integer destination, ElevatorCar elevatorCar) {
+
+        //1.check if destination is in the list of available floors
+
+        //2.submit the request to the jobDispatcher
+        dispatcher.submitInternalRequest(destination, elevatorCar);
+    }
+
+
 }
