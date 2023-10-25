51c477e8b6f7a409c30188204f40e016d076a821
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-25 23:59:16 +0530

    Adding java doc

--- elevator/src/main/java/com/lld/elevator/entities/ElevatorDisplay.java
+++ elevator/src/main/java/com/lld/elevator/entities/ElevatorDisplay.java
@@ -4,6 +4,11 @@ public class ElevatorDisplay {
     int floor;
     Direction direction;
 
+    /**
+     * Method to set display
+     * @param floor {@link Floor}
+     * @param direction {@link Direction}
+     */
     public void setDisplay(int floor, Direction direction) {
         this.floor = floor;
         this.direction = direction;
