522c93b4ba6fad35d32cea303ea9944c23144f31
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-25 23:58:15 +0530

    Adding java doc

--- elevator/src/main/java/com/lld/elevator/entities/Building.java
+++ elevator/src/main/java/com/lld/elevator/entities/Building.java
@@ -13,14 +13,26 @@ public class Building {
 
     private List<Floor> floorList;
 
+    /**
+     * Method to add floors
+     * @param newFloor {@link Floor}
+     */
     public void addFloors(Floor newFloor) {
         floorList.add(newFloor);
     }
 
+    /**
+     * Method to remove floors
+     * @param removeFloor {@link Floor}
+     */
     public void removeFloors(Floor removeFloor) {
         floorList.remove(removeFloor);
     }
 
+    /**
+     * Method to get Floor list
+     * @return {@link List<Floor>}
+     */
     List<Floor> getAllFloorList() {
         return floorList;
     }
