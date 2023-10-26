798a11385295110542193c7a9e7ccb2274e84387
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-26 20:05:33 +0530

    Adding java docs

--- elevator/src/main/java/com/lld/elevator/entities/ElevatorDoor.java
+++ elevator/src/main/java/com/lld/elevator/entities/ElevatorDoor.java
@@ -2,10 +2,16 @@ package com.lld.elevator.entities;
 
 public class ElevatorDoor {
 
+    /**
+     * Method to open door
+     */
     public void openDoor(){
         System.out.println("Opening the Elevator door ");
     }
 
+    /**
+     * Method to close door
+     */
     public void closeDoor(){
         System.out.println("Closing the Elevator door");
     }
