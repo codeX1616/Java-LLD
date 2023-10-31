1e103ee89288fe3a66a606d19bacc729525de26b
codeX1616 <pratikjoshi1616@gmail.com>
2023-10-31 23:01:25 +0530

    Creating Balance entity

---
+++ splitwise/src/main/java/com/example/splitwise/entity/Balance.java
@@ -0,0 +1,11 @@
+package com.example.splitwise.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Balance {
+    private Double amountOwe;
+    private Double amountGetBack;
+}
