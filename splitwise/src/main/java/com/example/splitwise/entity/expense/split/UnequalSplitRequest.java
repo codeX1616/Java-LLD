e3c0795c1d6f1c794e6a468467ccabf1133d6970
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-02 09:51:27 +0530

    Creating new implementation of UnequalSplitRequest

---
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/split/UnequalSplitRequest.java
@@ -0,0 +1,12 @@
+package com.example.splitwise.entity.expense.split;
+
+import java.util.List;
+
+public class UnequalSplitRequest implements ExpenseSplit {
+
+    @Override
+    public void validateSplitRequest(List<Split> splitList, double totalAmount) {
+
+    }
+
+}
