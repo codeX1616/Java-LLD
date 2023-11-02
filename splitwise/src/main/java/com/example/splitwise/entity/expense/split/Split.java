42ec51fbc40e56f90cb98ad1769ecfde07a65535
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-02 09:49:35 +0530

    Adding members and methods in Split class

--- splitwise/src/main/java/com/example/splitwise/entity/expense/split/Split.java
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/split/Split.java
@@ -1,4 +1,14 @@
 package com.example.splitwise.entity.expense.split;
 
+import com.example.splitwise.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Split {
+    User user;
+    double amountOwe;
 }
