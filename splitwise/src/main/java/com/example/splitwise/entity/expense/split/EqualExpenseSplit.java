73a3bc47b6863418ac8e4b1d1ccf99a36d2aae21
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-02 09:50:01 +0530

    Creating new class EqualExpenseSplit

---
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/split/EqualExpenseSplit.java
@@ -0,0 +1,12 @@
+package com.example.splitwise.entity.expense.split;
+
+import java.util.List;
+
+public class EqualExpenseSplit implements ExpenseSplit {
+
+    @Override
+    public void validateSplitRequest(List<Split> splitList, double totalAmount) {
+        // TODO - Add logic for validating split request
+    }
+
+}
