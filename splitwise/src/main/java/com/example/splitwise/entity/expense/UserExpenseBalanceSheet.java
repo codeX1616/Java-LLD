7e00a45304dc958035139afdf20f2c3716b84ce8
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-04 22:39:29 +0530

    Refactoring UserExpense Balance sheet

--- splitwise/src/main/java/com/example/splitwise/entity/expense/UserExpenseBalanceSheet.java
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/UserExpenseBalanceSheet.java
@@ -1,11 +1,25 @@
 package com.example.splitwise.entity.expense;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@Setter
 public class UserExpenseBalanceSheet {
     private Map<String, Balance> userVsBalance;
     private Double totalYourExpense;
     private Double totalPayment;
     private Double totalYouOwe;
     private Double totalYouGetBack;
+
+    public UserExpenseBalanceSheet(){
+        this.userVsBalance = new HashMap<>();
+        this.totalYourExpense = (double) 0;
+        this.totalYouOwe = (double) 0;
+        this.totalYouGetBack = (double) 0;
+    }
+
 }
