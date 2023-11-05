9472db970ea4bff964c80a2192613ff6618edf82
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-05 23:51:34 +0530

    Adding method show balance sheet of user

--- splitwise/src/main/java/com/example/splitwise/controller/BalanceSheetController.java
+++ splitwise/src/main/java/com/example/splitwise/controller/BalanceSheetController.java
@@ -1,4 +1,35 @@
 package com.example.splitwise.controller;
 
+import com.example.splitwise.entity.expense.Balance;
+import com.example.splitwise.entity.expense.UserExpenseBalanceSheet;
+import com.example.splitwise.entity.user.User;
+
+import java.util.Map;
+
 public class BalanceSheetController {
+
+    public void showBalanceSheetOfUser(User user){
+
+        System.out.println("---------------------------------------");
+
+        System.out.println("Balance sheet of user : " + user.getUserId());
+
+        UserExpenseBalanceSheet userExpenseBalanceSheet =  user.getUserExpenseBalanceSheet();
+
+        System.out.println("TotalYourExpense: " + userExpenseBalanceSheet.getTotalYourExpense());
+        System.out.println("TotalGetBack: " + userExpenseBalanceSheet.getTotalYouGetBack());
+        System.out.println("TotalYourOwe: " + userExpenseBalanceSheet.getTotalYouOwe());
+        System.out.println("TotalPaymnetMade: " + userExpenseBalanceSheet.getTotalPayment());
+        for(Map.Entry<String, Balance> entry : userExpenseBalanceSheet.getUserVsBalance().entrySet()){
+
+            String userID = entry.getKey();
+            Balance balance = entry.getValue();
+
+            System.out.println("userID:" + userID + " YouGetBack:" + balance.getAmountGetBack() + " YouOwe:" + balance.getAmountOwe());
+        }
+
+        System.out.println("---------------------------------------");
+
+    }
+
 }
