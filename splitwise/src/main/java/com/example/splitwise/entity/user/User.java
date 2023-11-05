a6f3f0e794df1459f67400710a7048ecd2709acf
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-05 23:51:07 +0530

    Adding Getters and Setters to User entity

--- splitwise/src/main/java/com/example/splitwise/entity/user/User.java
+++ splitwise/src/main/java/com/example/splitwise/entity/user/User.java
@@ -1,7 +1,11 @@
 package com.example.splitwise.entity.user;
 
 import com.example.splitwise.entity.expense.UserExpenseBalanceSheet;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class User {
     private String userId;
     private String userName;
