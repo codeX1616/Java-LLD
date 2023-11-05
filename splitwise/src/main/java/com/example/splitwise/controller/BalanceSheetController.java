e9b6ccbbaa62ed33a84cb30afa497cbc32a591c0
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-05 23:53:41 +0530

    Adding update user expense balance sheet method

--- splitwise/src/main/java/com/example/splitwise/controller/BalanceSheetController.java
+++ splitwise/src/main/java/com/example/splitwise/controller/BalanceSheetController.java
@@ -2,8 +2,10 @@ package com.example.splitwise.controller;
 
 import com.example.splitwise.entity.expense.Balance;
 import com.example.splitwise.entity.expense.UserExpenseBalanceSheet;
+import com.example.splitwise.entity.expense.split.Split;
 import com.example.splitwise.entity.user.User;
 
+import java.util.List;
 import java.util.Map;
 
 public class BalanceSheetController {
@@ -35,5 +37,56 @@ public class BalanceSheetController {
         System.out.println("---------------------------------------");
 
     }
+
+    public void updateUserExpenseBalanceSheet(User expensePaidBy, List<Split> splits, double totalExpenseAmount){
+
+        //update the total amount paid of the expense paid by user
+        UserExpenseBalanceSheet paidByUserExpenseSheet = expensePaidBy.getUserExpenseBalanceSheet();
+        paidByUserExpenseSheet.setTotalPayment(paidByUserExpenseSheet.getTotalPayment() + totalExpenseAmount);
+
+        for(Split split : splits) {
+
+            User userOwe = split.getUser();
+            UserExpenseBalanceSheet oweUserExpenseSheet = userOwe.getUserExpenseBalanceSheet();
+            double oweAmount = split.getAmountOwe();
+
+            if(expensePaidBy.getUserId().equals(userOwe.getUserId())){
+                paidByUserExpenseSheet.setTotalYourExpense(paidByUserExpenseSheet.getTotalYourExpense()+oweAmount);
+            }
+            else {
+
+                //update the balance of paid user
+                paidByUserExpenseSheet.setTotalYouGetBack(paidByUserExpenseSheet.getTotalYouGetBack() + oweAmount);
+
+                Balance userOweBalance;
+                if(paidByUserExpenseSheet.getUserVsBalance().containsKey(userOwe.getUserId())) {
+
+                    userOweBalance = paidByUserExpenseSheet.getUserVsBalance().get(userOwe.getUserId());
+                }
+                else {
+                    userOweBalance = new Balance();
+                    paidByUserExpenseSheet.getUserVsBalance().put(userOwe.getUserId(), userOweBalance);
+                }
+
+                userOweBalance.setAmountGetBack(userOweBalance.getAmountGetBack() + oweAmount);
+
+
+                //update the balance sheet of owe user
+                oweUserExpenseSheet.setTotalYouOwe(oweUserExpenseSheet.getTotalYouOwe() + oweAmount);
+                oweUserExpenseSheet.setTotalYourExpense(oweUserExpenseSheet.getTotalYourExpense() + oweAmount);
+
+                Balance userPaidBalance;
+                if(oweUserExpenseSheet.getUserVsBalance().containsKey(expensePaidBy.getUserId())){
+                    userPaidBalance = oweUserExpenseSheet.getUserVsBalance().get(expensePaidBy.getUserId());
+                }
+                else{
+                    userPaidBalance = new Balance();
+                    oweUserExpenseSheet.getUserVsBalance().put(expensePaidBy.getUserId(), userPaidBalance);
+                }
+                userPaidBalance.setAmountOwe(userPaidBalance.getAmountOwe() + oweAmount);
+            }
+        }
+    }
+
 
 }
