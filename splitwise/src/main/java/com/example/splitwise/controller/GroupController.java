0a8ca58e5128bdf20773051f81a5ef3e565b33be
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-06 22:51:52 +0530

    Refactoring Group Controller

--- splitwise/src/main/java/com/example/splitwise/controller/GroupController.java
+++ splitwise/src/main/java/com/example/splitwise/controller/GroupController.java
@@ -1,5 +1,16 @@
 package com.example.splitwise.controller;
 
+import com.example.splitwise.entity.user.Group;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupController {
+
+    private List<Group> groupList;
+
+    public GroupController(){
+        groupList = new ArrayList<>();
+    }
 
 }
