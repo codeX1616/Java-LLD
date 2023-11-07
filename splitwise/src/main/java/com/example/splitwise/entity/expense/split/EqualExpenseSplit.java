62ee3c92fa297c2c139af9244162df0189dbc944
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-07 23:58:16 +0530

    Adding java doc

--- splitwise/src/main/java/com/example/splitwise/entity/expense/split/EqualExpenseSplit.java
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/split/EqualExpenseSplit.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 public class EqualExpenseSplit implements ExpenseSplit {
 
+    /**
+     * Method to validate split request
+     * @param splitList {@link List<Split>}
+     * @param totalAmount {@link Double}
+     */
     @Override
-    public void validateSplitRequest(List<Split> splitList, double totalAmount) {
+    public void validateSplitRequest(List<Split> splitList, Double totalAmount) {
         double amountShouldBePresent = totalAmount/splitList.size();
         for(Split split: splitList) {
             if(split.getAmountOwe() != amountShouldBePresent) {
