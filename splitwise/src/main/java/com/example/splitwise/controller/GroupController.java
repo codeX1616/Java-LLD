d5e7e53f6e66e294be5d428a1d39e9f0febd3088
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-13 18:09:17 +0530

    Adding member and constructor to UserController

--- splitwise/src/main/java/com/example/splitwise/controller/GroupController.java
+++ splitwise/src/main/java/com/example/splitwise/controller/GroupController.java
@@ -52,7 +52,4 @@ public class GroupController {
         return null;
     }
 
-
-
-
 }
--- splitwise/src/main/java/com/example/splitwise/controller/UserController.java
+++ splitwise/src/main/java/com/example/splitwise/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.example.splitwise.controller;
 
+import com.example.splitwise.entity.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class UserController {
+
+    private List<User> userList;
+
+    public UserController(){
+        userList = new ArrayList<>();
+    }
+
+    //add user
+    public void addUser(User user) {
+        userList.add(user);
+    }
+
 
 }
