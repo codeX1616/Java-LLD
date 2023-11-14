df2f5bc84e213ee982e0875f0984c3599ebdb58c
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-14 20:21:58 +0530

    Refactoring switch case

--- splitwise/src/main/java/com/example/splitwise/entity/expense/split/SplitFactory.java
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/split/SplitFactory.java
@@ -5,16 +5,11 @@ import com.example.splitwise.entity.expense.ExpenseSplitType;
 public class SplitFactory {
 
     public static ExpenseSplit getSplitObject(ExpenseSplitType splitType) {
-        switch (splitType) {
-            case EQUAL:
-                return new EqualExpenseSplit();
-            case UNEQUAL:
-                return new UnequalExpenseSplit();
-            case PERCENTAGE:
-                return new PercentageExpenseSplit();
-            default:
-                return null;
-        }
+        return switch (splitType) {
+            case EQUAL -> new EqualExpenseSplit();
+            case UNEQUAL -> new UnequalExpenseSplit();
+            case PERCENTAGE -> new PercentageExpenseSplit();
+        };
     }
 
 }
