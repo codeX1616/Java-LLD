dbdf92a5e1527c315cf66a66703e2685ce4124fb
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-17 23:57:06 +0530

    Adding java doc

--- splitwise/src/main/java/com/example/splitwise/entity/expense/split/PercentageExpenseSplit.java
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/split/PercentageExpenseSplit.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class PercentageExpenseSplit implements ExpenseSplit {
 
+    /**
+     * Method to validate percentage split request
+     * @param splitList {@link List<Split>}
+     * @param totalAmount {@link Double}
+     */
     @Override
     public void validateSplitRequest(List<Split> splitList, Double totalAmount) {
 
