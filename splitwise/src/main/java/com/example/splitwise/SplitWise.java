e7f287c0d03d9d7a10b2968b8bd5995858ade60b
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-22 22:04:25 +0530

    Adding expense input

--- splitwise/src/main/java/com/example/splitwise/SplitWise.java
+++ splitwise/src/main/java/com/example/splitwise/SplitWise.java
@@ -4,6 +4,7 @@ import com.example.splitwise.controller.BalanceSheetController;
 import com.example.splitwise.controller.GroupController;
 import com.example.splitwise.controller.UserController;
 import com.example.splitwise.entity.user.Group;
+import com.example.splitwise.entity.expense.split.Split;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -29,6 +30,16 @@ public class SplitWise {
         Group group = groupController.getGroup("G1001");
         group.addMember(userController.getUser("U2001"));
         group.addMember(userController.getUser("U3001"));
+
+        //Step2. create an expense inside a group
+        List<Split> splits = new ArrayList<>();
+        Split split1 = new Split(userController.getUser("U1001"), 300);
+        Split split2 = new Split(userController.getUser("U2001"), 300);
+        Split split3 = new Split(userController.getUser("U3001"), 300);
+        splits.add(split1);
+        splits.add(split2);
+        splits.add(split3);
+        group.createExpense("Exp1001", "Breakfast", 900, splits, ExpenseSplitType.EQUAL, userController.getUser("U1001"));
 
     }
 
