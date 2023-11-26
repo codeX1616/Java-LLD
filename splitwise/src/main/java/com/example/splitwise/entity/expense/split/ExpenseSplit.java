3ec85f7f692c0aaebd2a51ec459c9238ca6a798b
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-26 15:29:22 +0530

    Adding java doc to split request method

--- splitwise/src/main/java/com/example/splitwise/entity/expense/split/ExpenseSplit.java
+++ splitwise/src/main/java/com/example/splitwise/entity/expense/split/ExpenseSplit.java
@@ -1,8 +1,15 @@
 package com.example.splitwise.entity.expense.split;
 
 import java.util.List;
+import com.example.splitwise.entity.expense.split.Split
 
 public interface ExpenseSplit {
+
+    /**
+     * Interface Method to validate split Request
+     * @param splitList {@link List<Split>}
+     * @param totalAmount {@link Double}
+     */
     public void validateSplitRequest(List<Split> splitList, Double totalAmount);
 
 }
