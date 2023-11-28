9fe661cfd5124a182606ea5c49c1727a67f85989
codeX1616 <pratikjoshi1616@gmail.com>
2023-11-28 23:28:01 +0530

    Adding java doc

--- splitwise/src/main/java/com/example/splitwise/SplitwiseApplication.java
+++ splitwise/src/main/java/com/example/splitwise/SplitwiseApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SplitwiseApplication {
 
+	/**
+	 * Entry point for spring boot java application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SplitwiseApplication.class, args);
 		SplitWise splitWise = new SplitWise();
